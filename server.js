f3aed685ef48a151bc7c1c99d27e19f8f5c4a518
Ryan Kelly <ryan@rfk.id.au>
2013-02-05 15:31:11 +1100

    Tweaks to enable deployment via awsbox

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var routes = require('./routes');
 var settings = {};
 
 // Create a server with a host and port
-var server = new Hapi.Server('localhost', process.env.PORT || 8000, settings);
+var port = parseInt(process.env.PORT || 8000, 10);
+var server = new Hapi.Server('localhost', port, settings);
 server.addRoutes(routes);
 
 module.exports = server;
