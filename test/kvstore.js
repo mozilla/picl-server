425d97813ab501fcec64707ff785247a121a26a1
Ryan Kelly <rkelly@mozilla.com>
2013-02-19 15:59:29 +1100

    Implement synchronous connection API for kvstore.

--- test/kvstore.js
+++ test/kvstore.js
@@ -5,32 +5,30 @@ var config = require('../lib/config');
 describe('kvstore', function () {
 
   it('can set and retrieve keys', function (done) {
-    kvstore.connect(config.get('kvstore'), function(err, db) {
-      db.set("test-key", "VALUE", function(err) {
+    var db = kvstore.connect(config.get('kvstore'));
+    db.set("test-key", "VALUE", function(err) {
+      assert.equal(err, null);
+      db.get("test-key", function(err, info) {
         assert.equal(err, null);
-        db.get("test-key", function(err, info) {
-          assert.equal(err, null);
-          assert.equal(info.value, "VALUE");
-          done();
-        });
+        assert.equal(info.value, "VALUE");
+        done();
       });
     });
   });
 
   it('supports atomic check-and-set', function (done) {
-    kvstore.connect(config.get('kvstore'), function(err, db) {
-      db.set("test-key", "VALUE", function(err) {
-        db.get("test-key", function(err, info) {
-          assert.equal(info.value, "VALUE");
-          db.cas("test-key", "OTHER-VALUE-ONE", info.casid, function(err) {
-            assert.equal(err, null);
-            db.cas("test-key", "OTHER-VALUE-TWO", info.casid, function(err) {
-              assert.notEqual(err, null);
-              db.get("test-key", function(err, info) {
-                assert.equal(err, null);
-                assert.equal(info.value, "OTHER-VALUE-ONE");
-                done();
-              });
+    var db = kvstore.connect(config.get('kvstore'));
+    db.set("test-key", "VALUE", function(err) {
+      db.get("test-key", function(err, info) {
+        assert.equal(info.value, "VALUE");
+        db.cas("test-key", "OTHER-VALUE-ONE", info.casid, function(err) {
+          assert.equal(err, null);
+          db.cas("test-key", "OTHER-VALUE-TWO", info.casid, function(err) {
+            assert.notEqual(err, null);
+            db.get("test-key", function(err, info) {
+              assert.equal(err, null);
+              assert.equal(info.value, "OTHER-VALUE-ONE");
+              done();
             });
           });
         });
