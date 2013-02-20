8c209779f83de1ca4aabd5b05c9206cdbfe5ca1f
Zachary Carter <zack.carter@gmail.com>
2013-02-19 16:19:10 -0800

    allow clients to use an email address instead of an assertion for updating or deleting tokens; use sync kvstore api

--- test/integration/blob.js
+++ test/integration/blob.js
@@ -6,27 +6,13 @@ var server = helpers.server;
 var makeRequest = helpers.makeRequest.bind(server);
 
 var TEST_AUDIENCE = config.get('public_url');
-var TEST_EMAIL;
-var TEST_ASSERTION;
-var TEST_TOKEN = 'foobar';
+var TEST_EMAIL = 'blob@example.com';
+var TEST_TOKEN = 'blobtoken';
 
 describe('set up account', function() {
-  it('can get user email and assertion', function(done) {
-    helpers.getUser(TEST_AUDIENCE, function(err, user) {
-
-      TEST_EMAIL = user.email;
-      TEST_ASSERTION = user.assertion;
-
-      assert.ok(TEST_EMAIL);
-      assert.ok(TEST_ASSERTION);
-
-      done();
-    });
-  });
-
   it('creates a new account', function(done) {
     makeRequest('PUT', '/update_token', {
-      payload: { assertion: TEST_ASSERTION, token: TEST_TOKEN, oldTokens: [ 'not', 'used' ] }
+      payload: { email: TEST_EMAIL, token: TEST_TOKEN, oldTokens: [ 'not', 'used' ] }
     }, function(res) {
       assert.equal(res.statusCode, 200);
       assert.deepEqual(res.result, { success: true, email: TEST_EMAIL });
@@ -62,7 +48,6 @@ describe('blob', function() {
       headers: { Authorization: 'bad' }
     }, function(res) {
       assert.equal(res.statusCode, 401);
-      //assert.deepEqual(res.result, { success: true, data: 'my awesome data', casid: 2 });
       done();
     });
   });
---
+++ test/integration/fakeAuth.js
@@ -0,0 +1,73 @@
+var assert = require('assert');
+var config = require('../../lib/config');
+var helpers = require('../helpers');
+
+var server = helpers.server;
+var makeRequest = helpers.makeRequest.bind(server);
+
+var TEST_EMAIL = 'foo@example.com';
+var TEST_TOKEN = 'fake';
+var TEST_NEW_TOKEN = 'fakenew';
+
+describe('fake auth', function() {
+  it('creates a new account', function(done) {
+    makeRequest('PUT', '/update_token', {
+      payload: { email: TEST_EMAIL, token: TEST_TOKEN, oldTokens: [ 'not', 'used' ] }
+    }, function(res) {
+      assert.equal(res.statusCode, 200);
+      assert.deepEqual(res.result, { success: true, email: TEST_EMAIL });
+      done();
+    });
+  });
+
+  it('fails to update new token when old token is invalid', function(done) {
+    makeRequest('PUT', '/update_token', {
+      payload: { email: TEST_EMAIL, token: TEST_NEW_TOKEN, oldTokens: [ 'bad' ] }
+    }, function(res) {
+      assert.equal(res.statusCode, 401);
+      assert.equal(res.result.message, 'KnownUserUnknownToken');
+      done();
+    });
+  });
+
+  it('updates new token when an old token is valid', function(done) {
+    makeRequest('PUT', '/update_token', {
+      payload: { email: TEST_EMAIL, token: TEST_NEW_TOKEN, oldTokens: [ 'bad', TEST_TOKEN ] }
+    }, function(res) {
+      assert.equal(res.statusCode, 200);
+      assert.deepEqual(res.result, { success: true, email: TEST_EMAIL });
+      done();
+    });
+  });
+
+  it('succeeds using the current token', function(done) {
+    makeRequest('PUT', '/update_token', {
+      payload: { email: TEST_EMAIL, token: TEST_NEW_TOKEN }
+    }, function(res) {
+      assert.equal(res.statusCode, 200);
+      assert.deepEqual(res.result, { success: true, email: TEST_EMAIL });
+      done();
+    });
+  });
+
+  it('deletes the account', function(done) {
+    makeRequest('DELETE', '/account', {
+      payload: { email: TEST_EMAIL }
+    }, function(res) {
+      assert.equal(res.statusCode, 200);
+      assert.deepEqual(res.result, { success: true });
+      done();
+    });
+  });
+
+  it('creates a new account with a new token', function(done) {
+    makeRequest('PUT', '/update_token', {
+      payload: { email: TEST_EMAIL, token: 'fake new token' }
+    }, function(res) {
+      assert.equal(res.statusCode, 200);
+      assert.deepEqual(res.result, { success: true, email: TEST_EMAIL });
+      done();
+    });
+  });
+});
+
